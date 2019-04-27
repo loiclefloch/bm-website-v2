3675bae4851a90c2e24d1b176bd352cf49b0db9b
Loic <l.lefloch.91@gmail.com>
2019-04-27 17:10:37 +0200

    update mock-api list files

--- plugins/reacticoon-mock-api-cli-plugin/actions/listFiles.js
+++ plugins/reacticoon-mock-api-cli-plugin/actions/listFiles.js
@@ -1,25 +1,9 @@
-// List all files in a directory in Node.js recursively in a synchronous fashion
-// https://gist.github.com/kethinov/6658166
-// TODO: handle subfolders
-var walkSync = function(dir, filelist) {
-  var fs = fs || require('fs'),
-    files = fs.readdirSync(dir)
-  filelist = filelist || []
-  files.forEach(function(file) {
-    if (fs.statSync(dir + file).isDirectory()) {
-      filelist = walkSync(dir + file + '/', filelist)
-    } else {
-      filelist.push({ file, dir })
-    }
-  })
-  return filelist
-}
+const Filesystem = require('create-reacticoon-app/utils/Filesystem')
 
 function listFiles() {
   return {
-    TEST: 42,
     '//': 'It works :D',
-    walk: walkSync(__dirname + '/../../../test/api-mocks/'),
+    tree: Filesystem.getTree(__dirname + '/../../../test/api-mocks/'),
   }
 }
 
